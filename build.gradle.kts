3539fb2a3acb5637b9e04bb041d01f5fcb358558
Olavo Barros Meira Filho <olavomeiradeveloper@gmail.com>
2026-02-15 17:32:40 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "br.com.softhouse.dende"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.github.lasilva:dendeframework:1.0.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
